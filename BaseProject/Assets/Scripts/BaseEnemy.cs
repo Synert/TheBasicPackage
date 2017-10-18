4d22f5648477852e7805a90ae5aac4a612b0b207
Connor Rhone <Connor2.Rhone@live.uwe.ac.uk>
2017-10-18 10:43:14 +0100

    Added a basic enemy prefab, and further folders.

---
+++ BaseProject/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+
+    int direction = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Physics.Raycast(transform.position, direction * Vector3.right, 0.5f))
+        {
+            direction *= -1;
+        }
+        else if (!Physics.Raycast(transform.position, direction * Vector3.right - Vector3.up, 0.5f))
+        {
+            direction *= -1;
+        }
+    }
+}
